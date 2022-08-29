a5ae9a00b26592726b0ed5ebb145bc396b98fb79
nirmalbachhav20 <111499013+nirmalbachhav20@users.noreply.github.com>
2022-08-29 15:47:31 +0530

    Add files via upload

---
+++ FibonacciNumber/FibonacciNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            int y = 1;
+
+            int z = 1;
+            Console.WriteLine("Fibonacci Series ----------------");
+
+
+
+            while (x <= 100)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+                Console.WriteLine(y);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
+
+
