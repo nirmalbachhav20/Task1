6af8d4b97eeaf8ba09c180b3699cadf0552c3c7a
nirmalbachhav20 <111499013+nirmalbachhav20@users.noreply.github.com>
2022-08-29 15:45:06 +0530

    Add files via upload

---
+++ PersonalInfoLib/PersonDataTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalInfoLib;
+
+namespace PersonDataTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person person1 = new Person("Nirmal", "Bachhav", "NB@gmail.com", Convert.ToDateTime("26/09/1997"));
+
+            Console.WriteLine(person1);
+
+
+            Console.WriteLine("Is person is Adult " + person1.IsAdult());
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ PersonalInfoLib/PersonalInfoLib/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalInfoLib
+{
+    public class Person
+    {
+        private string firstname; // camel case
+
+
+        public string FirstName
+        {
+            get { return firstname; }
+            set { firstname = value; }
+
+
+        }
+
+        private string lastname; // camel case
+
+
+        public string LastName
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+
+        private string email; // camel case
+
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+
+
+
+        private DateTime birthdate; // camel case
+
+
+        public DateTime BirthDate
+        {
+            get { return birthdate; }
+            set { birthdate = value; }
+        }
+        public bool IsAdult()
+        {
+            int age = DateTime.Now.Year - birthdate.Year;
+
+            if (age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
+
+
+    public Person(string _firstname, string _lastname, string _email, DateTime _birthdate)
+        {
+            this.FirstName = _firstname;
+            this.LastName = _lastname;
+            this.Email = _email;
+            this.BirthDate = _birthdate;
+        }
+        public override string ToString()
+        {
+            return String.Format($"firstname : {FirstName} lastname : {LastName} email : {Email} birthdate : {BirthDate}");
+        }
+
+
+    }
+}
