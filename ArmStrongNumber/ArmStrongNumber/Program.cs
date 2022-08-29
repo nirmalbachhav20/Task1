163102cd2482b8edd543dc376a221f1a0d308650
nirmalbachhav20 <111499013+nirmalbachhav20@users.noreply.github.com>
2022-08-29 15:45:46 +0530

    Add files via upload

---
+++ ArmStrongNumber/ArmStrongNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArmStrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            Console.WriteLine("ArmStrongNumbers From 1 to 1000.....");
+            for (int i = 1; i <= 1000; i++)
+        {
+                a = i / 100;
+                b = (i - a * 100) / 10;
+                c = (i - a * 100 - b * 10);
+                d = a * a * a + b * b * b + c * c * c;
+                if (i == d)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
