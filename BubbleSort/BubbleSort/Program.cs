dbf5604dc82c57d30772e23e60d545c6dc5cd183
nirmalbachhav20 <111499013+nirmalbachhav20@users.noreply.github.com>
2022-08-29 15:46:59 +0530

    Add files via upload

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 12, 11, 6, 2, 1, 25 };
+            int a;
+
+
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                for (int j = 0; j < num.Length - 1 - i; j++)
+                {
+                    if (num[j] > num[j + 1])
+                    {
+                        a = num[j];
+                        num[j] = num[j + 1];
+                        num[j + 1] = a;
+                    }
+                }
+            }
+
+
+
+            foreach (int b in num)
+            {
+                Console.Write(b + " ");
+            }
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+    }
+    
