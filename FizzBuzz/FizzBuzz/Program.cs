82042405eb397cd1d5760d3a4d8f4f3730043254
nirmalbachhav20 <111499013+nirmalbachhav20@users.noreply.github.com>
2022-08-29 15:46:21 +0530

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+
+           
+
+            // Loop for 100 times
+            for (int i = 1; i <= n; i++)
+            {
+
+                
+                if (i % 3 == 0)
+                    Console.WriteLine("Fizz" + " ");
+
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz" + " ");
+
+                else if (i % 9 == 0)
+                    Console.WriteLine("FizzBuzz" + " ");
+
+                // Print the numbers
+                else
+                    Console.WriteLine(i + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
